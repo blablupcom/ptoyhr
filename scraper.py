58870b1632e88d1d3c2883a732723c85929ca41f
blablupcom <o.volodin@yahoo.com>
2016-01-04 02:02:13 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -97,7 +97,7 @@ def parse(url):
                             except:
                                 pass
                             today_date = str(datetime.now())
-                            print asin, amazon_price
+                            # print asin, amazon_price
                             # return asin, amazon_price
 
                             scraperwiki.sqlite.save(unique_keys=['Date'], data={'ASIN': asin, 'Date': today_date, 'Amazon Price': amazon_price, 'Total Offer Count': total_offer_count, 'Lowest Price': lowest_price, 'link': l+'?&pg={}'.format(i)})
