bc46be380d20a2f5dced3ab6074eb062124a4aab
blablupcom <o.volodin@yahoo.com>
2016-01-04 05:32:31 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -119,6 +119,6 @@ def parse(url):
 import grequests
 
 if __name__ == '__main__':
-            pool = Pool(cpu_count() * 2)
+            pool = Pool(cpu_count() * 1)
             asins = pool.map(parse, start_urls)
             # for asin_link in asins:
