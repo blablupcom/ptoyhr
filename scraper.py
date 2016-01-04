2e0fbc0eed1ecc978e7112f313426e93f3548336
blablupcom <o.volodin@yahoo.com>
2016-01-04 03:56:47 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -116,5 +116,5 @@ if __name__ == '__main__':
     sites = []
     for u in start_urls:
         rs = grequests.get(u, hooks=dict(response=parse))
-        job = grequests.send(req, grequests.Pool(39))
+        job = grequests.send(rs, grequests.Pool(39))
     # grequests.map(sites)
