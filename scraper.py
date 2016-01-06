b45da007090a1f72a34d7cc700f9f731945300ee
blablupcom <o.volodin@yahoo.com>
2016-01-06 12:16:59 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,106 +1,91 @@
-import requests
+iimport requests
 from bs4 import BeautifulSoup as bs
-import scraperwiki
-from datetime import datetime
-import urllib2
-import grequests
-
-start_urls = ['http://www.amazon.com/Best-Sellers-Appliances/zgbs/appliances/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Appstore-Android/zgbs/mobile-apps/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Arts-Crafts-Sewing/zgbs/arts-crafts/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Automotive/zgbs/automotive/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Baby/zgbs/baby-products/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Beauty/zgbs/beauty/ref=zg_bs_nav_0',
-'http://www.amazon.com/best-sellers-books-Amazon/zgbs/books/ref=zg_bs_nav_0',
-'http://www.amazon.com/best-sellers-camera-photo/zgbs/photo/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Cell-Phones-Accessories/zgbs/wireless/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Clothing/zgbs/apparel/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Collectible-Coins/zgbs/coins/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Computers-Accessories/zgbs/pc/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Electronics/zgbs/electronics/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Entertainment-Collectibles/zgbs/entertainment-collectibles/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Gift-Cards/zgbs/gift-cards/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Grocery-Gourmet-Food/zgbs/grocery/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Health-Personal-Care/zgbs/hpc/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Home-Kitchen/zgbs/home-garden/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Home-Improvement/zgbs/hi/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Industrial-Scientific/zgbs/industrial/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Jewelry/zgbs/jewelry/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Kindle-Store/zgbs/digital-text/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Kitchen-Dining/zgbs/kitchen/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-MP3-Downloads/zgbs/dmusic/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Magazines/zgbs/magazines/ref=zg_bs_nav_0',
-'http://www.amazon.com/best-sellers-movies-TV-DVD-Blu-ray/zgbs/movies-tv/ref=zg_bs_nav_0',
-'http://www.amazon.com/best-sellers-music-albums/zgbs/music/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Musical-Instruments/zgbs/musical-instruments/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Office-Products/zgbs/office-products/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Patio-Lawn-Garden/zgbs/lawn-garden/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Pet-Supplies/zgbs/pet-supplies/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Prime-Pantry/zgbs/pantry/ref=zg_bs_nav_0',
-'http://www.amazon.com/best-sellers-shoes/zgbs/shoes/ref=zg_bs_nav_0',
-'http://www.amazon.com/best-sellers-software/zgbs/software/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Sports-Outdoors/zgbs/sporting-goods/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Sports-Collectibles/zgbs/sports-collectibles/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Toys-Games/zgbs/toys-and-games/ref=zg_bs_nav_0',
-'http://www.amazon.com/best-sellers-video-games/zgbs/videogames/ref=zg_bs_nav_0',
-'http://www.amazon.com/Best-Sellers-Watches/zgbs/watches/ref=zg_bs_nav_0']
+import re
+from mutagen.mp3 import MP3
+from urllib import urlretrieve
+import json
+import math
+import itertools
 
 
-def scrape(response, **kwargs):
+def scrape_page(base_url):
+        page = requests.get(base_url)
+        soup = bs(page.text, 'lxml')
+        links = soup.find('table', 'archives').find_all('a')
+        for link in links:
+            title_link = 'http://www.dailywav.com'+link['href']
+            title_page = requests.get(title_link)
+            title_soup = bs(title_page.text, 'lxml')
+            files_links = title_soup.find('div', 'view-content').find_all('a')
+            for files_link in files_links:
+                movie_link = 'http://www.dailywav.com'+files_link['href']
+                movie_name = files_link.text.strip()
+                movie_page = requests.get(movie_link)
+                movie_soup = bs(movie_page.text, 'lxml')
+                image_url = ''
+                try:
+                    image_url = movie_soup.find('div', 'coverArt2').find('img')['src']
+                except:
+                    try:
+                        image_url = movie_soup.find('div', 'content quotes').find('img')['src']
+                    except:
+                        pass
 
+                nodes = movie_soup.find_all('div', 'node-content-wrapper')
+                for node in nodes:
+                    file_url = node.find('div', 'content quotes').find('a')['href']
+                    transcript = node.find('div', 'content quotes').find('p').text.strip()
+                    file_type = file_url.split('.')[-1]
+                    categories = 'Shows'
+                    # try:
+                    #     filename, headers = urlretrieve(link)
+                    #     audio = MP3(filename)
+                    #     if audio.info.length < 1:
+                    #         length = int(math.ceil(audio.info.length))
+                    #     else:
+                    #         length = int(round(audio.info.length))
+                    #     duration = length
+                    # except:
+                    #     duration = ''
+                    print movie_link, movie_name, transcript,  file_type, file_url, categories, image_url
+                    yield movie_link, movie_name, transcript,  file_type, file_url, categories, image_url
 
-        listing_soup = bs(response.text, 'lxml')
-        asin_nums = listing_soup.find_all('div', 'zg_itemImmersion')
-        for asin_num in asin_nums:
-            asin = ''
-            try:
-                asin = asin_num.find('a')['href'].split('dp/')[-1].strip()
-            except:
-                pass
-            amazon_price = ''
-            try:
-                amazon_price = asin_num.find('strong', 'price').text.strip()
-            except:
-                pass
-            total_offer_count = ''
-            try:
-                total_offer_count = asin_num.find('div', 'zg_usedPrice').find('a').text.strip().split(u'\xa0')[0].replace('used & new', '')
-            except:
-                pass
-            lowest_price = ''
-            try:
-                lowest_price = asin_num.find('div', 'zg_usedPrice').find('span', 'price').text.strip()
-            except:
-                pass
-            today_date = str(datetime.now())
-        #     return asin
-        #     return asin, today_date, amazon_price, total_offer_count, lowest_price
-            scraperwiki.sqlite.save(unique_keys=['Date'], data={'ASIN': asin, 'Date': today_date, 'Amazon Price': amazon_price, 'Total Offer Count': total_offer_count, 'Lowest Price': lowest_price})
+                next_link = ''
+                try:
+                    next_link = movie_soup.find('li', 'pager-next').find('a')
+                except:
+                    pass
+                if next_link:
+                    movie_page = requests.get('http://www.dailywav.com'+next_link)
+                    movie_soup = bs(movie_page.text, 'lxml')
+                    continue
+                else:
+                    break
 
 
 
+def scrape():
+   with open('data7.json', 'w') as outfile:
+        sounds = {}
+        lists = []
+        base_url = 'http://www.dailywav.com/archives/shows/'
+        s = scrape_page(base_url)
+        for l in s:
+            json_dic = {}
+            json_dic['sourceUrl']=l[0]
+            json_dic['movie name']=l[1]
+            json_dic['transcript']=l[2]
+            json_dic['fileType']=l[3]
+            json_dic['fileUrl']=l[4]
+            # json_dic['duration']=l[6]
+            json_dic['categories']=l[5]
+            json_dic['imageUrl']=l[5]
+            print json_dic['movie name']
+            lists.append(json_dic)
+        sounds['sounds'] = lists
+        json.dump(sounds, outfile, indent = 4)
 
-def parse(url):
 
-    page = urllib2.urlopen(url).read()
 
-    soup = bs(page, 'lxml')
-    try:
-        active_sel = soup.find('span', 'zg_selected').find_next()
-        if active_sel.name == 'ul':
-            links_lists= active_sel.find_all('li')
-
-            for links in links_lists:
-                        l = links.find('a')['href'].encode('utf-8')
-                        async_list = []
-                        for i in xrange(1, 6):
-                            print l+'?&pg={}'.format(i)
-                            rs = (grequests.get(l+'?&pg={}'.format(i), hooks = {'response' : scrape}))
-                            async_list.append(rs)
-                        parse(l)
-                        grequests.map(async_list)
-    except:
-        parse(url)
 if __name__ == '__main__':
-    for start_url in start_urls:
-        parse(start_url)
+    scrape()
